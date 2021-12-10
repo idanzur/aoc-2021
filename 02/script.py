5f9aba6d08206ad9520e56b95ae28c272924485c
idanzur <idanzur97@gmail.com>
2021-12-10 16:16:48 +0200

    day 2 part 2

--- 02/script.py
+++ 02/script.py
@@ -19,7 +19,24 @@ def part1():
     print(f'part1: {pos * depth}')
 
 def part2():
-    pass
+    with open(file) as f:
+        data = f.read()
+    pos = 0
+    depth = 0
+    aim = 0
+    for row in data.splitlines():
+        cmd, x = row.split(' ')
+        x = int(x)
+        if cmd == 'down':
+            aim += x
+        elif cmd == 'up':
+            aim -= x
+        else:
+            pos += x
+            depth += x * aim
+    
+    print(f'part2: {pos * depth}')
+
 
 if __name__  == '__main__':
-    part1()
+    part2()
