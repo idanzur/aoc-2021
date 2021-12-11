a3fbcd6e546add4e14a520abff4c76e706d55e5a
idanzur <idanzur97@gmail.com>
2021-12-11 09:11:57 +0200

    day 11 part 1

---
+++ 11/script.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python3
+from dataclasses import dataclass
+from typing import List, Tuple
+
+file = './sample.txt' if 0 else './input.txt'
+
+CROSSED_FORMAT = '\033[1m{}\033[0m'
+
+@dataclass
+class Cell:
+    val: int
+    flashed: bool = False
+    def __repr__(self) -> str:
+        if self.flashed:
+            return CROSSED_FORMAT.format(self.val)
+        return str(self.val)
+
+class Grid:
+    SIZE = 10
+    def __init__(self, cells: List[List[Cell]]):
+        self.cells = cells
+        self.flashes = 0
+
+    def step(self):
+        for y, row in enumerate(self.cells):
+            for x, cell in enumerate(row):
+                cell.val += 1
+        change = True
+        while change:
+            change = False
+            for y, row in enumerate(self.cells):
+                for x, cell in enumerate(row):
+                    if cell.val > 9:
+                        change = True
+                        self.flash(x, y)
+                        cell.flashed = True
+                        cell.val = 0
+                        self.flashes += 1
+    def clean(self):
+        for y, row in enumerate(self.cells):
+            for x, cell in enumerate(row):
+                cell.flashed = False
+        
+
+    def flash(self, x, y):        
+        for _x in range(x - 1, x + 2):
+            for _y in range(y - 1, y + 2):
+                if 0 <= _x < self.SIZE and 0 <= _y < self.SIZE and (_x != x or _y != y):
+                    cell = self.cells[_y][_x]
+                    if not cell.flashed:
+                        cell.val += 1
+
+
+
+
+    def __repr__(self) -> str:
+        res = ""
+        for row in self.cells:
+            for cell in row:
+                res += str(cell)
+            res += '\n'
+        return res
+
+
+def part1():
+    with open(file) as f:
+        data = f.read()
+    cells = []
+    for row in data.splitlines():
+        t = []
+        for n in row:
+            t.append(Cell(val=int(n)))
+        cells.append(t)
+    
+    grid = Grid(cells)
+    for i in range(100):
+        grid.step()
+        grid.clean()
+    print(f'part1: {grid.flashes}')
+def part2():
+    pass
+
+if __name__  == '__main__':
+    part1()
