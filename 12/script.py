2979b843739cb023c0dd3420b3feae9aac438195
idanzur <idanzur97@gmail.com>
2021-12-12 12:15:39 +0200

    day 12 part 2

--- 12/script.py
+++ 12/script.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-from os import name, path
 from typing import List
-from dataclasses import dataclass
 from pprint import pprint
 
 
@@ -19,16 +17,21 @@ class Cave:
         return f'{self.name}'
 
 paths = []
-def find_path(start: Cave, visited: List[Cave]):
+def find_path(start: Cave, visited: List[Cave], allowed_twice: Cave = None):
     global paths
     if start.name == 'end':
-        paths.append(visited + [start])
+        new_path = visited + [start]
+        if new_path not in paths:
+            paths.append(new_path)
     for cave in start.paths:
-        if cave.name.lower() == cave.name and cave in visited:
-            continue
-        find_path(cave, visited + [start])
-        
-def part1():
+        if cave.name.lower() == cave.name:
+            if cave is allowed_twice and visited.count(cave) < 2:
+                pass
+            elif cave in visited:
+                continue
+        find_path(cave, visited + [start], allowed_twice)
+
+def load_data():
     with open(file) as f:
         data = f.read()
     caves = {}
@@ -40,12 +43,19 @@ def part1():
         cave_b.add(cave_a)
         caves[name_a] = cave_a
         caves[name_b] = cave_b
-    
+    return caves
+
+def part1():
+    caves = load_data()
     find_path(caves['start'], [])
     print(f'part1: {len(paths)}')
 
 def part2():
-    pass
+    caves = load_data()
+    for name, cave in caves.items():
+        if name.lower() == name and name not in ['start', 'end']:
+            find_path(caves['start'], [], cave)
+    print(f'part2: {len(paths)}')
 
 if __name__  == '__main__':
-    part1()
+    part2()
