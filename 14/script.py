5d3b415e51755952fdfda66ed18ec03fab4d87e0
idanzur <idanzur97@gmail.com>
2021-12-14 07:32:40 +0200

    day 14 part 1

---
+++ 14/script.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+import re
+from collections import Counter
+from pprint import pprint
+
+file = './sample.txt' if 0 else './input.txt'
+
+def part1():
+    with open(file) as f:
+        data = f.read()
+    template, data = data.split('\n\n')
+    rules = []
+    for row in data.split('\n'):
+        rules.append(row.split(' -> '))
+
+    for _ in range(10):
+        matches = []
+        for pattern, val in rules:
+            for match in re.finditer(f'(?={pattern})', template):
+                # print(pattern, (match.start(), val))
+                matches.append((match.start() + 1, val))
+        
+        matches.sort(reverse=True)
+        # print(matches)
+        for index, val in matches:
+            template = template[:index] + val + template[index:]
+    
+    vals_counter = Counter(template)
+    vals = vals_counter.values()
+    max_val = max(vals)
+    min_val = min(vals)
+    res = max_val - min_val
+    print(f'part1: {res}')
+
+
+def part2():
+    pass
+
+if __name__  == '__main__':
+    part1()
