56442af6548e1274c301265edd0cd7c8dc77a3aa
idanzur <idanzur97@gmail.com>
2021-12-17 17:31:45 +0200

    python3.10 updates

--- 16/script.py
+++ 16/script.py
@@ -12,8 +12,7 @@ class Packet:
     type: int
     version: int
     sub_packets: List['Packet'] = None
-    payload: int = None
-    
+    value: int = None
 
 
 class Parser:
@@ -23,27 +22,27 @@ class Parser:
             data = f.read()
         self.buffer = bin(int(data, 16))[2:].zfill(len(data) * 4)
 
-    def chop(self, size):
+    def chop(self, size: int) -> str:
         res = self.buffer[:size]
         self.buffer = self.buffer[size:]
         return res
 
-    def chop_int(self, size):
+    def chop_int(self, size: int) -> int:
         return int(self.chop(size), 2)
 
-    def parse(self):
+    def parse(self) -> Packet:
         p_version = self.chop_int(3)
         p_type = self.chop_int(3)
         if p_type == 4:
-            p_payload = ""
+            p_value = ""
             while True:
                 last = self.chop_int(1)
                 chunck = self.chop(4)
-                p_payload += chunck
+                p_value += chunck
                 if last == 0:
                     break
-            p_payload = int(p_payload, 2)
-            return Packet(type=p_type, version=p_version, payload=p_payload)
+            p_value = int(p_value, 2)
+            return Packet(type=p_type, version=p_version, value=p_value)
         len_type_id = self.chop_int(1)
         if len_type_id == 1:
             sub_packet_count = self.chop_int(11)
@@ -58,29 +57,30 @@ class Parser:
         return Packet(type=p_type, version=p_version, sub_packets=sub_packets)
 
 
-def sum_versions(root: Packet):
+def sum_versions(root: Packet) -> int:
     if root.type == 4:
         return root.version
     return root.version + sum([sum_versions(p) for p in root.sub_packets])
 
 
-def calc(root: Packet):
-    if root.type == 0:
-        return sum(map(calc, root.sub_packets))
-    if root.type == 1:
-        return reduce(mul, map(calc, root.sub_packets))
-    if root.type == 2:
-        return min(map(calc, root.sub_packets))
-    if root.type == 3:
-        return max(map(calc, root.sub_packets))
-    if root.type == 4:
-        return root.payload
-    if root.type == 5:
-        return int(calc(root.sub_packets[0]) > calc(root.sub_packets[1]))
-    if root.type == 6:
-        return int(calc(root.sub_packets[0]) < calc(root.sub_packets[1]))
-    if root.type == 7:
-        return int(calc(root.sub_packets[0]) == calc(root.sub_packets[1]))
+def calc(root: Packet) -> int:
+    match root.type:
+        case 0:
+            return sum(map(calc, root.sub_packets))
+        case 1:
+            return reduce(mul, map(calc, root.sub_packets))
+        case 2:
+            return min(map(calc, root.sub_packets))
+        case 3:
+            return max(map(calc, root.sub_packets))
+        case 4:
+            return root.value
+        case 5:
+            return int(calc(root.sub_packets[0]) > calc(root.sub_packets[1]))
+        case 6:
+            return int(calc(root.sub_packets[0]) < calc(root.sub_packets[1]))
+        case 7:
+            return int(calc(root.sub_packets[0]) == calc(root.sub_packets[1]))
 
 
 def part1():
