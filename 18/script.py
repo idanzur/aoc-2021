5d8a06b537d17298a60082ccacb3db6f4fb2c2c4
idanzur <idanzur97@gmail.com>
2021-12-18 23:52:40 +0200

    refactor day 18

--- 18/script.py
+++ 18/script.py
@@ -5,109 +5,88 @@ from copy import deepcopy
 file = './sample.txt' if 0 else './input.txt'
 
 
-def find_explode(query):
-    global found, explode_path
-    found = False
-    explode_path = None
+class Reducer:
+    def __init__(self, number):
+        self.number = number
+        self.path = None
+        self.found = False
 
-    def inner(query, n, path):
-        global found, explode_path
-        if type(query[0]) is int and type(query[1]) is int:
+    def reduce(self):
+        while self.explode() or self.split():
+            self.found = False
+            self.path = None
+        return self.number
+
+    def explode(self):
+        self.find_explode(self.number, 0, [])
+        if self.path:
+            left_item, right_item = self.get_at(self.path)
+            right_path = self.find_near_number(0)
+            if right_path:
+                self.add_at(right_path, right_item)
+            left_path = self.find_near_number(1)
+            if left_path:
+                self.add_at(left_path, left_item)
+            self.modify_at(self.path, 0)
+
+        return bool(self.path)
+
+    def find_explode(self, number, n, path):
+        if type(number[0]) is int and type(number[1]) is int:
             if n == 4:
-                found = True
-                explode_path = path
+                self.found = True
+                self.path = path
             return
-        for i, item in enumerate(query):
-            if type(item) is list and not found:
-                inner(item, n + 1, path + [i])
-
-    return inner(query, 0, [])
-
-
-def find_number(query, path, dir):
-    if dir not in path:
-        return
-    last_pos = len(path) - path[::-1].index(dir) - 1
-    start_path = path[: last_pos] + [1-dir]
-    for i in start_path:
-        query = query[i]
-
-    while query != []:
-        if type(query) is int:
-            return start_path
-        start_path.append(dir)
-        query = query[dir]
-
-
-def modify_at(query, path, val):
-    for i in path[:-1]:
-        query = query[i]
-    query[path[-1]] = val
-
-
-def add_at(query, path, val):
-    for i in path[:-1]:
-        query = query[i]
-    query[path[-1]] += val
-
-
-def get_at(query, path):
-    for i in path:
-        query = query[i]
-    return query
-
-
-def find_split(query):
-    global found, split_path
-    found = False
-    split_path = None
-
-    def inner(query, path):
-        global found, split_path
-        for i, item in enumerate(query):
-            if found:
-                continue
+        for i, item in enumerate(number):
+            if self.found: return
             if type(item) is list:
-                inner(item, path + [i])
+                self.find_explode(item, n + 1, path + [i])
+
+    def find_near_number(self, dir):
+        path = self.path
+        temp = self.number
+        if dir not in path:
+            return
+        last_pos = len(path) - path[::-1].index(dir) - 1
+        start_path = path[: last_pos] + [1-dir]
+
+        temp = self.get_at(start_path)
+
+        while temp != []:
+            if type(temp) is int:
+                return start_path
+            start_path.append(dir)
+            temp = temp[dir]
+
+    def split(self):
+        self.find_split(self.number, [])
+        if self.path:
+            item = self.get_at(self.path)
+            self.modify_at(self.path, [item//2, math.ceil(item/2)])
+        return bool(self.path)
+
+    def find_split(self, number, path):
+        for i, item in enumerate(number):
+            if self.found: return
+            if type(item) is list:
+                self.find_split(item, path + [i])
             elif item >= 10:
-                found = True
-                split_path = path + [i]
+                self.found = True
+                self.path = path + [i]
 
-    return inner(query, [])
+    def modify_at(self, path, val):
+        temp = self.get_at(path[:-1])
+        temp[path[-1]] = val
 
+    def add_at(self, path, val):
+        temp = self.get_at(path[:-1])
+        temp[path[-1]] += val
 
-def explode(query):
-    global explode_path
-    find_explode(query)
-    if explode_path:
-        left_item, right_item = get_at(query, explode_path)
-        right_path = find_number(query, explode_path, 0)
-        if right_path:
-            add_at(query, right_path, right_item)
-        left_path = find_number(query, explode_path, 1)
-        if left_path:
-            add_at(query, left_path, left_item)
-        modify_at(query, explode_path, 0)
-
-    return bool(explode_path)
-
-
-def split(query):
-    find_split(query)
-    if split_path:
-        item = get_at(query, split_path)
-        modify_at(query, split_path, [item//2, math.ceil(item/2)])
-    return bool(split_path)
-
-
-def calculate(query):
-    exploded = splited = True
-    while exploded or splited:
-        exploded = splited = True
-        while exploded:
-            exploded = explode(query)
-        splited = split(query)
-    return query
+    def get_at(self, path):
+        temp = self.number
+        for i in path:
+            temp = temp[i]
+        return temp
 
 
 def magnitude(query):
@@ -116,34 +95,29 @@ def magnitude(query):
     return 3 * magnitude(query[0]) + 2 * magnitude(query[1])
 
 
-def part1():
-    global explode_path, split_path
+def load_data():
     with open(file) as f:
         data = f.read()
+    return list(map(eval, data.splitlines()))
 
-    rows = list(map(eval, data.splitlines()))
+
+def part1():
+    rows = load_data()
     start = rows[0]
     for row in rows[1:]:
-        start = calculate([start, row])
-
-    ans = (magnitude(start))
+        start = Reducer([start, row]).reduce()
+    ans = magnitude(start)
     print(f'part1: {ans}')
 
 
 def part2():
-    global explode_path, split_path
-    with open(file) as f:
-        data = f.read()
-
-    rows = list(map(eval, data.splitlines()))
+    rows = load_data()
     max_mag = 0
     for a in rows:
         for b in rows:
             if a != b:
-                calc = calculate([deepcopy(a), deepcopy(b)])
-                new_mag = magnitude(calc)
-                if new_mag > max_mag:
-                    max_mag = new_mag
+                calc = Reducer([deepcopy(a), deepcopy(b)]).reduce()
+                max_mag = max(magnitude(calc), max_mag)
     print(f'part2: {max_mag}')
 
 
