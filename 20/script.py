f1a760a9b65b55683fcb89b8124584afa9edc67f
idanzur <idanzur97@gmail.com>
2021-12-21 00:21:08 +0200

    organize code

--- 20/script.py
+++ 20/script.py
@@ -24,15 +24,14 @@ class Enhancer:
             for _x in range(x - 1, x + 2):
                 res += '1' if self.grid[(_x, _y)] == '#' else '0'
         return int(res, 2)
-    
+
     def count(self, offset):
         s = 0
         for y in range(self.start_y-offset, self.end_y + 1 + offset):
             for x in range(self.start_x-offset, self.end_x + 1 + offset):
-                if self.grid[(x,y)] == '#':
+                if self.grid[(x, y)] == '#':
                     s += 1
-        return s      
-
+        return s
 
 
 def load_data():
@@ -45,25 +44,23 @@ def load_data():
             grid[(x, y)] = cell
     return grid, lookup
 
+def enhance_for(loops):
+    grid, lookup = load_data()
+    enhancer = Enhancer(grid, lookup)
+    for _ in range(loops):
+        enhancer.enhance(loops * 3)
+    return enhancer.count(loops * 2)
 
 def part1():
-    grid, lookup = load_data()
-    enhancer = Enhancer(grid, lookup)
-    loops = 2
-    for _ in range(loops):
-        enhancer.enhance(loops * 3)
-    res = enhancer.count(loops * 2)
+    res = enhance_for(2)
     print(f'part1: {res}')
 
+
 def part2():
-    grid, lookup = load_data()
-    enhancer = Enhancer(grid, lookup)
-    loops = 50
-    for _ in range(loops):
-        enhancer.enhance(loops * 3)
-    res = enhancer.count(loops * 2)
+    res = enhance_for(50)
     print(f'part2: {res}')
 
-if __name__  == '__main__':
+
+if __name__ == '__main__':
     part1()
     part2()
